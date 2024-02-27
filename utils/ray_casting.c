b6d9edfb6d80605d031f2a92e8e19447460bd700
Benjamin WACH <bwach@MacBook-Air-de-Benjamin.local>
2024-02-27 01:04:36 +0100

    Verifcation 2.0

--- utils/ray_casting.c
+++ utils/ray_casting.c
@@ -6,11 +6,39 @@
 /*   By: bwach <bwach@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 12:03:18 by bwach             #+#    #+#             */
-/*   Updated: 2024/02/26 23:36:17 by bwach            ###   ########.fr       */
+/*   Updated: 2024/02/27 01:03:18 by bwach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+static int	ray(t_map *m, t_play *p, int offset, int i)
+{
+	if (offset != 0)
+	{
+		if (i == 1)
+		{
+			if (m->map[(p->bot_r[1]) / 64][(p->bot_r[0] + offset) / 64] == '1')
+				return (ray(m, p, offset - 1, 1));
+			else
+				return (offset);
+		}
+		else if (i == 2)
+		{
+			if (m->map[(p->bot_l[1]) / 64][(p->bot_l[0] - offset) / 64] == '1')
+				return (ray(m, p, offset - 1, 2));
+			else
+				return (offset);
+		}
+		else if (i == 3)
+		{
+			if (m->map[(p->bot_l[1] + offset) / 64][(p->bot_l[0]) / 64] == '1')
+				return (ray(m, p, offset - 1, 2));
+			return (offset);
+		}
+	}
+	return (0);
+}
 
 int	dist_to_right(t_map *m, t_play *p)
 {
@@ -28,7 +56,8 @@ int	dist_to_right(t_map *m, t_play *p)
 	if (m->map[(p->bot_r[1] / 64)][(p->bot_r[0] + 4) / 64] == '1' ||
 		m->map[(p->top_r[1] / 64)][(p->top_r[0] + 4) / 64] == '1')
 	{
-		dist_to_wall = (p->bot_r[0] + 4) / 64 - (p->bot_r[0] / 64);
+		dist_to_wall = (p->bot_r[0] + (ray(m, p, 4, 1))) / 64
+			- (p->bot_r[0] / 64);
 		if (m->map[(p->top_r[1] / 64)][(p->top_r[0] + 4) / 64] == '1' ||
 			m->map[(p->bot_r[1] / 64)][(p->bot_r[0] + 4) / 64] == '1')
 			return (dist_to_wall);
@@ -54,12 +83,15 @@ int	dist_to_left(t_map *m, t_play *p)
 	if (m->map[(p->bot_l[1] / 64)][(p->bot_l[0] - 4) / 64] == '1' ||
 		m->map[(p->top_l[1] / 64)][(p->top_l[0] - 4) / 64] == '1')
 	{
-		dist_to_wall = (p->bot_l[0] - 4) / 64 - (p->bot_l[0] / 64);
+		dist_to_wall = (p->bot_l[0] - (ray(m, p, 4, 2))) / 64
+			- (p->bot_l[0] / 64);
 		if (m->map[(p->top_l[1] / 64)][(p->top_l[0] + 4) / 64] == '1' ||
 			m->map[(p->bot_l[1] / 64)][(p->bot_l[0] + 4) / 64] == '1')
 			return (dist_to_wall);
 		else if (dist_to_wall > 4)
 			return (4);
+		else
+			return (dist_to_wall);
 	}
 	return (4);
 }
@@ -95,7 +127,8 @@ int	dist_to_bot(t_map *m, t_play *p)
 	if (m->map[(p->bot_r[1] + 4) / 64][(p->bot_r[0]) / 64] == '1' ||
 		m->map[(p->bot_l[1] + 4) / 64][(p->bot_l[0]) / 64] == '1')
 	{
-		dist_to_wall = (p->bot_l[1] + 4) / 64 - (p->bot_l[1] / 64);
+		dist_to_wall = (p->bot_l[1] + (ray(m, p, 4, 3))) / 64
+			- (p->bot_l[1] / 64);
 		if (m->map[(p->bot_l[1] + 4) / 64][(p->bot_l[0]) / 64] == '1' ||
 			m->map[(p->bot_r[1] + 4) / 64][(p->bot_r[0]) / 64] == '1')
 			return (dist_to_wall);
